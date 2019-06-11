d31e0ffe1b42ab9e833b6d2a1e305d8f7cb3d41b
lingan <saboran@163.com>
2019-06-11 18:18:59 +0800

    static file & swoole http server

--- server/http-server.php
+++ server/http-server.php
@@ -1,2 +1,19 @@
 <?php
 
+$host = "0.0.0.0";
+$port = 9090;
+
+$static_root = __DIR__ . '/../resources/live';
+
+$http = new swoole_http_server($host, $port);
+
+// 开启静态资源处理
+$http->set([
+    'enable_static_handler' => true,
+    'document_root'         => $static_root,
+]);
+
+$http->on('request', function (swoole_http_request $request, swoole_http_response $response) {
+});
+
+$http->start();
