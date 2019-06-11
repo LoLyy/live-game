f6c11a6a43be64cddf8d7af726882c0285e7ca81
lingan <saboran@163.com>
2019-06-11 17:50:29 +0800

    static file

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/detail', function () {
+    return view('detail');
+});
+Route::get('/login', function () {
+    return view('login');
+});
